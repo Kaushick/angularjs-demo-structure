71bc1072fe713876ff99e6fead589156aa50f460
indranil_mondal <indranil.mondal@innofied.com>
2014-11-05 16:57:39 +0530

    Initial architecture

--- app/app.js
+++ app/app.js
@@ -4,6 +4,6 @@ var angular = require('angular'),
 util = require('./util/util'),
 angularRoute = require('angular-route'),
 uiBootstrap = require('angular-bootstrap'),
-
+avengerModule = require('./modules/avengers/avengers.module'),
 App = angular.module('app', ['app.avengers']);
 module.exports = App;
--- app/modules/avengers/avengers.module.js
+++ app/modules/avengers/avengers.module.js
@@ -4,6 +4,5 @@ var angular = require('angular'),
 util = require('./util/util'),
 angularRoute = require('angular-route'),
 uiBootstrap = require('angular-bootstrap'),
-avengerModule = require('./modules/avengers/avengers.module'),
 Avengers = angular.module('app.avengers', []);
 module.exports = Avengers;
